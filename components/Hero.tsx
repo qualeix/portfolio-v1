34c2d931095b83571f7af151765822a11591fa04
qualeix <qualeix@gmail.com>
2024-05-03 17:18:30 +0200

    Mobile revisions

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div id="Hero">
       <div className="flex flex-wrap">
         <motion.div
-          className="flex w-full flex-col place-content-center items-center lg:w-1/2 lg:items-start lg:p-12 lg:pl-40"
+          className="order-2 flex w-full flex-col place-content-center items-center lg:order-1 lg:w-1/2 lg:items-start lg:p-12 lg:pl-40"
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1 }}
@@ -17,7 +17,7 @@ const Hero = () => {
           <p className="gradient-cyber mb-8 h-14 animate-gradient text-5xl font-medium text-transparent">
             CyberSécurité
           </p>
-          <div className="flex gap-4">
+          <div className="flex flex-col justify-center gap-4 lg:justify-start">
             <a
               href="#contact"
               className="inline-flex select-none p-3 font-semibold underline"
@@ -40,9 +40,9 @@ const Hero = () => {
           initial={{ y: 50, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.75, delay: 1 }}
-          className="w-full lg:w-1/2 lg:pr-14"
+          className="order-1 w-full lg:order-2 lg:w-1/2 lg:pr-14"
         >
-          <div className="flex justify-center">
+          <div className="flex justify-center pb-12 lg:pb-0">
             <Image unoptimized src={Ball} alt="Hero GIF" draggable="false" />
           </div>
         </motion.div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 const Navbar = () => {
-  return <div className="mb-20 py-6"></div>;
+  return <div className="mb-10 py-3 lg:mb-20 lg:py-6"></div>;
 };
 
 export default Navbar;
